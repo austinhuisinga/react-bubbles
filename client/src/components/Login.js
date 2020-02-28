7a16ba0586983ebf9da82202dd3095206cbc482f
austinhuisinga <57517058+austinhuisinga@users.noreply.github.com>
2020-02-28 09:26:17 -0700

    Step 1 - Authentication Complete.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { useForm } from 'react-hook-form';
 
-const Login = () => {
+
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { handleSubmit, register, errors } = useForm({});
+
+  const onSubmit = e => {
+    axiosWithAuth()
+      .post('/api/login', {
+        username: username,
+        password: password,
+      })
+      .then(res => {
+        console.log(res);
+        localStorage.setItem('token', res.data.payload);
+        props.history.push('/');
+      })
+      .catch(err => console.log('ERRORRRRR', err))
+  };
+
+  const handleChange = e => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor='username'>Username</label>
+      <input
+        id='username'
+        name='username'
+        type='text'
+        onChange={handleChange}
+        ref={register({
+          required: 'Username required!'
+        })}
+      />
+      {errors.username && errors.username.message}
+
+      <label htmlFor='password'>Password</label>
+      <input
+        id='password'
+        name='password'
+        type='password'
+        onChange={handleChange}
+        ref={register({
+          required: 'Password required -- like, obviously.'
+        })}
+      />
+      {errors.password && errors.password.message}
+
+      <button type='submit'>Login</button>
+    </form>
   );
 };
 
